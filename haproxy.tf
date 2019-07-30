034393d77505ca3f044f17f0af48cebfd7b3dca1
Stan Dorsett <stan.dorsett@gmail.com>
2019-07-30 22:22:36 +0000

    update haproxy.tf with multiple changes

--- haproxy.tf
+++ haproxy.tf
@@ -1,32 +1,34 @@
 resource "openstack_compute_instance_v2" "haproxy" {
   name            = "haproxy"
-  image_id        = "${data.openstack_images_image_v2.ubuntu_18_04.id}"
-  flavor_id       = "${data.openstack_compute_flavor_v2.s1-2.id}"
-  key_pair        = "${data.openstack_compute_keypair_v2.deploy-keypair.name}"
-  security_groups = ["${openstack_compute_secgroup_v2.deploy-k8s-allow-external-6443.name}"]
+  image_id        = data.openstack_images_image_v2.ubuntu_18_04.id
+  flavor_id       = data.openstack_compute_flavor_v2.sandbox.id
+  key_pair        = data.openstack_compute_keypair_v2.deploy-keypair.name
+  security_groups = [openstack_compute_secgroup_v2.deploy-k8s-allow-external-6443.name]
+  user_data       = templatefile("${path.module}/cloud-init.tpl", { private_ip = "10.240.0.9" })
 
   network {
-    name = "Ext-Net",
+    name = "Ext-Net"
   }
 
   network {
-    name = "${data.openstack_networking_network_v2.default-internal.name}"
+    name        = data.openstack_networking_network_v2.default-internal.name
     fixed_ip_v4 = "10.240.0.9"
   }
 
-  metadata {
-    deploy-k8s = "haproxy"
+  metadata = {
+    group = "haproxies"
   }
 
   provisioner "remote-exec" {
     inline = [
-      "sudo hostname -f",
+      "hostname -f",
     ]
     connection {
+      host        = coalesce(self.access_ip_v4, self.access_ip_v6)
       type        = "ssh"
-      user        = "${var.ssh_user}"
-      private_key = "${file(var.private_key)}"
+      user        = var.ssh_user
+      private_key = file(var.private_key)
     }
   }
-
 }
+
