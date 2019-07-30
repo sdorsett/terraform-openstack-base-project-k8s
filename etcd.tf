fe4614f6c811818646da06337a64d1201d27ce67
Stan Dorsett <stan.dorsett@gmail.com>
2019-07-30 22:21:37 +0000

    updated etcd.tf to make image name more generic

--- etcd.tf
+++ etcd.tf
@@ -2,7 +2,7 @@ resource "openstack_compute_instance_v2" "etcd" {
   count           = 3
   name            = "etcd-${count.index}"
   image_id        = data.openstack_images_image_v2.ubuntu_18_04.id
-  flavor_id       = data.openstack_compute_flavor_v2.s1-2.id
+  flavor_id       = data.openstack_compute_flavor_v2.sandbox.id
   key_pair        = data.openstack_compute_keypair_v2.deploy-keypair.name
   security_groups = [openstack_compute_secgroup_v2.deploy-k8s-allow-external-ssh.name]
   user_data       = templatefile("${path.module}/cloud-init.tpl", { private_ip = "10.240.0.3${count.index}" })
