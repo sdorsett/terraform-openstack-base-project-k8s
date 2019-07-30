c61f0df838f77c0d456b81d9c9d8545839e3f2c7
Stan Dorsett <stan.dorsett@gmail.com>
2019-07-30 22:24:35 +0000

    updated network.tf to move image and flavor resources into it

--- network.tf
+++ network.tf
@@ -1,7 +1,17 @@
-resource "openstack_compute_keypair_v2" "deploy-k8s-keypair" {
-  name       = "deploy-k8s-keypair"
-  public_key = "${file(var.public_key)}"
-  depends_on = ["null_resource.generate-sshkey"]
+provider "openstack" {
+}
+
+data "openstack_images_image_v2" "ubuntu_18_04" {
+  name        = "Ubuntu 18.04"
+  most_recent = true
+}
+
+data "openstack_compute_flavor_v2" "sandbox" {
+  name = "s1-8"
+}
+
+data "openstack_compute_keypair_v2" "deploy-keypair" {
+  name = "deploy-keypair"
 }
 
 resource "openstack_compute_secgroup_v2" "deploy-k8s-allow-external-6443" {
@@ -42,7 +52,6 @@ resource "openstack_compute_secgroup_v2" "deploy-k8s-allow-external-6443" {
     ip_protocol = "udp"
     cidr        = "10.240.0.0/24"
   }
-
 }
 
 resource "openstack_compute_secgroup_v2" "deploy-k8s-allow-external-ssh" {
@@ -76,14 +85,14 @@ resource "openstack_compute_secgroup_v2" "deploy-k8s-allow-external-ssh" {
     ip_protocol = "udp"
     cidr        = "10.240.0.0/24"
   }
-
 }
 
-data "openstack_networking_network_v2" "infra-internal" {
-  name           = "${var.network-identifier}"
+data "openstack_networking_network_v2" "default-internal" {
+  name = var.network-identifier
 }
 
-data "openstack_networking_subnet_v2" "infra-internal-subnet" {
-  name       = "${var.network-identifier}"
+data "openstack_networking_subnet_v2" "default-internal-subnet" {
+  name       = var.network-identifier
   ip_version = 4
 }
+
